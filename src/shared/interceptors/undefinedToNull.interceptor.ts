109050334e3e162013c38689e7a49177dccd20de
smilejakdu <ash982416@gmail.com>
2023-06-13 20:58:27 +0900

    first settings

---
+++ src/shared/interceptors/undefinedToNull.interceptor.ts
@@ -0,0 +1,14 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class UndefinedToNullInterceptor implements NestInterceptor {
+	intercept(
+		context: ExecutionContext,
+		next: CallHandler<any>,
+	): Observable<any> | Promise<Observable<any>> {
+		// controller 전 부분
+		return next.handle().pipe(map((data) => data === undefined ? null : data));
+	}
+}
---
+++ src/shared/swaggerConfig.ts
@@ -0,0 +1,15 @@
+import {INestApplication} from "@nestjs/common";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+
+export const SwaggerSetting = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('dealver API')
+    .setDescription('dealver Swagger')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+};
