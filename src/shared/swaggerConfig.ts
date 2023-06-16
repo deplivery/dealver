f490f6f84ea69d745fda18b62c2f1225ad43b27d
smilejakdu <ash982416@gmail.com>
2023-06-16 20:10:34 +0900

    swaggerSettings title과 description을 watcha 에서 dealver 로 수정

--- src/shared/swaggerConfig.ts
+++ src/shared/swaggerConfig.ts
@@ -1,14 +1,13 @@
-import { INestApplication } from "@nestjs/common";
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export const SwaggerSetting = (app: INestApplication) => {
   const config = new DocumentBuilder()
-    .setTitle('nestWatcha API')
-    .setDescription('nestWatcha Swagger')
+    .setTitle('dealver API')
+    .setDescription('dealver Swagger')
     .setVersion('1.0')
     .addCookieAuth('connect.sid')
     .build();
-
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/v1', app, document);
