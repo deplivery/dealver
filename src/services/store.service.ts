a8b0d146645597f994bb9846515d3584d27d7215
player31-kks <keumks789@naver.com>
2023-06-23 22:40:36 +0900

    [Add] store create test case

--- src/services/store.service.ts
+++ src/services/store.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { StoreRepository } from '../repository/store.repository';
+import { PermissionError } from '../shared/error/permission.error';
+import { InputError } from '../shared/error/input.error';
+
+interface CreateStoreInput {
+  name: string;
+  address: string;
+  startHour: number;
+  endHour: number;
+}
 
 @Injectable()
 export class StoreService {
-  createStore() {
+  constructor(private readonly storeRepository: StoreRepository) {}
+
+  createStore(user: { role: string }, input: CreateStoreInput): boolean {
+    if (user.role !== 'admin' && user.role !== 'owner') {
+      throw new PermissionError('권한이 없습니다.');
+    }
+    if (input.startHour >= input.endHour) {
+      throw new InputError('영업시간이 영업 마감시간보다 늦습니다.');
+    }
+    const existStore = this.storeRepository.findByAddress(input.address);
+    if (existStore) {
+      throw new InputError('이미 존재하는 가게입니다.');
+    }
     return true;
   }
 }
--- src/services/store.spec.ts
+++ src/services/store.spec.ts
@@ -1,23 +1,83 @@
+import { StoreRepository } from './../repository/store.repository';
 import { Test } from '@nestjs/testing';
 import { StoreService } from './store.service';
+import { MockedValueProvider, mockProvider } from '../../test/util/mock';
+import { Store } from '../entities/store.entity';
+import { PermissionError } from '../shared/error/permission.error';
+import { InputError } from '../shared/error/input.error';
 
 describe('store', () => {
   let service: StoreService;
+  let storeRepository: MockedValueProvider<StoreRepository>;
 
   beforeAll(async () => {
+    storeRepository = mockProvider(StoreRepository);
     const app = (
       await Test.createTestingModule({
         imports: [],
-        providers: [StoreService],
+        providers: [StoreService, storeRepository],
       }).compile()
     ).createNestApplication();
     await app.init();
     service = app.get<StoreService>(StoreService);
   });
 
+  beforeAll(() => {
+    storeRepository.useValue.findByAddress.mockImplementation(() => undefined);
+  });
+
   describe('가게 생성', () => {
-    it('가게는 관리자와 점주만 생성할수 있다.', () => {
-      expect(service.createStore()).toBe(true);
+    it('가게는 관리자와 점주만 생성할수 있다.', async () => {
+      const user = { role: 'admin' };
+      const input = { name: 'test', address: 'test', startHour: 1, endHour: 2 };
+      const result = await service.createStore(user, input);
+      expect(result).toBe(true);
+    });
+
+    it('가게는 관리자와 점주만 생성할수 있다.', async () => {
+      const user = { role: 'owner' };
+      const input = { name: 'test', address: 'test', startHour: 1, endHour: 2 };
+      const result = await service.createStore(user, input);
+      expect(result).toBe(true);
+    });
+
+    it('가게는 관리자와 점주만 생성할수 있다.', async () => {
+      const user = { role: 'customer' };
+      const input = { name: 'test', address: 'test', startHour: 1, endHour: 2 };
+      expect(async () => {
+        await service.createStore(user, input);
+      }).rejects.toThrowError(new PermissionError('권한이 없습니다.'));
+    });
+
+    it('가게 정보에서 영업시간 < 영업 마감시간 생성할수 있다.', async () => {
+      const user = { role: 'owner' };
+      const input = { name: 'test', address: 'test', startHour: 1, endHour: 2 };
+      const result = await service.createStore(user, input);
+      expect(result).toBe(true);
+    });
+
+    it('가게 정보에서 영업시간 >= 영업 마감시간 생성할수 없다.', async () => {
+      const user = { role: 'owner' };
+      const input = { name: 'test', address: 'test', startHour: 3, endHour: 2 };
+      expect(async () => {
+        await service.createStore(user, input);
+      }).rejects.toThrowError(new InputError('영업시간이 영업 마감시간보다 늦습니다.'));
+    });
+
+    it('가게 주소는 unique 해야한다.', async () => {
+      const user = { role: 'owner' };
+      const input = { name: 'test', address: 'test', startHour: 1, endHour: 2 };
+      const result = await service.createStore(user, input);
+      expect(result).toBe(true);
+    });
+
+    it('가게 주소가 존재하면 가게를 생성할수 없다.', async () => {
+      const user = { role: 'owner' };
+      const input = { name: 'test', address: 'test', startHour: 1, endHour: 2 };
+      storeRepository.useValue.findByAddress.mockImplementation(async () => new Store());
+      expect(async () => {
+        await service.createStore(user, input);
+      }).rejects.toThrowError(new InputError('이미 존재하는 가게입니다.'));
     });
   });
 });
