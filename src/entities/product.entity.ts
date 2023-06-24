edf687a26d946765694ac025e8760266b05d0792
Nhahan <kisy324@naver.com>
2023-06-24 23:37:42 +0900

    feat: product entity

--- src/entities/product.entity.ts
+++ src/entities/product.entity.ts
@@ -1,10 +1,30 @@
+import { CoreEntity } from '../shared/core.entity';
+import { Column, Entity } from 'typeorm';
+
 export enum PRODUCT_STATUS {
   AVAILABLE = 'Available',
   UNAVAILABLE = 'Unavailable',
 }
 
-export class Product {
+@Entity()
+export class Product extends CoreEntity {
+  @Column()
+  storeManagerId: number;
+  @Column()
   name: string;
+  @Column()
+  price: number;
+  @Column()
   status: PRODUCT_STATUS;
+  @Column()
   count: number;
+
+  constructor(storeManagerId: number, name: string, price: number, status: PRODUCT_STATUS, count: number) {
+    super();
+    this.storeManagerId = storeManagerId;
+    this.name = name;
+    this.price = price;
+    this.status = status;
+    this.count = count;
+  }
 }
