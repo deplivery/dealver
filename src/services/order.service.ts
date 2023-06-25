bd97e37d203e01f8e96dff161db0315a443ae230
Nhahan <kisy324@naver.com>
2023-06-26 00:03:19 +0900

    todo: productId 로 storeId 가져오는 쿼리 필요

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -7,6 +7,7 @@ export class OrderService {
   constructor(private readonly orderRepository: OrderRepository) {}
 
   async createOrder(input: CreateOrderInput): Promise<Order> {
+    // TODO: productId 로 storeId 가져오는 쿼리 필요
     return this.orderRepository.createOrder(Order.of(input));
   }
 
