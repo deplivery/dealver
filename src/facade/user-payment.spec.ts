43aa042140047e80ba531ae59eb5c0d48d04cae9
Nhahan <kisy324@naver.com>
2023-06-27 00:47:33 +0900

    test: user-payment.facade.ts의 메소드 테스트

--- src/facade/user-payment.spec.ts
+++ src/facade/user-payment.spec.ts
@@ -10,6 +10,8 @@ import { User } from '../entities/user.entity';
 import { PAYMENT_TYPE } from '../entities/payment.entity';
 import { RequestFailError } from '../shared/error/request-fail.error';
 import { RedisService } from '../infra/redis.service';
+import { ORDER_STATUS } from '../entities/order.entity';
+import { OrderDetail } from '../entities/order-detail.entity';
 
 describe('order-payment', () => {
   let orderService: MockedValueProvider<OrderService>;
@@ -50,10 +52,15 @@ describe('order-payment', () => {
     userPaymentFacade = app.get<UserPaymentFacade>(UserPaymentFacade);
   });
 
-  describe('registerUserPayment', () => {
+  describe('registerUserPayment() 테스트', () => {
     let isAvailable = true;
-    const user = {} as User;
+    let user = {} as User;
     let valueFromRedis;
+
+    const userId = 1;
+    const paymentType = PAYMENT_TYPE.KAKAO;
+    const productCounts = [];
+
     beforeAll(() => {
       productService.useValue.checkAvailableProducts.mockImplementation(async (productCounts: ProductCount[]) => {
         return isAvailable;
@@ -69,14 +76,66 @@ describe('order-payment', () => {
 
     it('주문한 음식들이 주문 가능하지 않을 때, RequestFailError 던짐', async () => {
       isAvailable = false;
-      const userId = 1;
-      const paymentType = PAYMENT_TYPE.KAKAO;
-      const productCounts = [];
       await expect(userPaymentFacade.registerUserPayment(userId, paymentType, productCounts)).rejects.toThrow(
         RequestFailError,
       );
     });
 
-    it('주문한 음식들이 주문 가능하지만, 유효하지 않은 유저일 때, RequestFailError 던짐', async () => {});
+    it('주문한 음식들이 주문 가능하지만, 유효하지 않은 유저일 때, RequestFailError 던짐', async () => {
+      user = undefined;
+      await expect(userPaymentFacade.registerUserPayment(userId, paymentType, productCounts)).rejects.toThrow(
+        RequestFailError,
+      );
+    });
+  });
+
+  describe('processUserPayment() 테스트', () => {
+    const key = 'key';
+    const user = { id: 1 };
+    const productCounts = [{ productId: 1, count: 5 }];
+    const rawValue = JSON.stringify({ user, info: productCounts });
+
+    const order = { id: 1, userId: user.id, storeId: 1, status: ORDER_STATUS.PAID };
+    const orderDetail: OrderDetail = { id: 1, orderId: 1, productId: 1, count: 5 };
+
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('Redis에 배달 정보가 없을 때, RequestFailError 던짐', async () => {
+      cacheService.useValue.getValue.mockResolvedValue(null);
+      await expect(userPaymentFacade.processUserPayment(key)).rejects.toThrow(RequestFailError);
+      expect(cacheService.useValue.getValue).toHaveBeenCalledTimes(1);
+    });
+
+    it('주문한 음식들이 주문 가능하지만, 유효하지 않은 order일 때, RequestFailError 던짐', async () => {
+      const parsedValue = JSON.parse(rawValue);
+      const user = parsedValue.user;
+
+      cacheService.useValue.getValue.mockResolvedValueOnce(rawValue); // getValue 메서드의 반환값 설정
+      orderService.useValue.createOrder.mockResolvedValueOnce(null); // createOrder 메서드의 반환값 설정
+
+      await expect(userPaymentFacade.processUserPayment(key)).rejects.toThrow(RequestFailError);
+
+      expect(cacheService.useValue.getValue).toHaveBeenCalledWith(key);
+      expect(orderService.useValue.createOrder).toHaveBeenCalledWith({
+        userId: user.id,
+        status: ORDER_STATUS.PAID,
+      });
+
+      expect(orderDetailService.useValue.createOrderDetail).not.toHaveBeenCalled();
+    });
+
+    it('정상적인 주문 상황일 때, OrderDetail[]을 리턴함', async () => {
+      cacheService.useValue.getValue.mockResolvedValue(rawValue);
+      orderService.useValue.createOrder.mockResolvedValue(order);
+      orderDetailService.useValue.createOrderDetail.mockResolvedValue(Promise.resolve(orderDetail));
+
+      const result = await userPaymentFacade.processUserPayment(key);
+      expect(result).toEqual([orderDetail]);
+      expect(orderService.useValue.createOrder).toHaveBeenCalledTimes(1);
+      expect(orderDetailService.useValue.createOrderDetail).toHaveBeenCalledTimes(productCounts.length);
+      expect(cacheService.useValue.getValue).toHaveBeenCalledTimes(1);
+    });
   });
 });
