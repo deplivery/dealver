2fa3dc0461e60a8ed1f3de32ad463f402bcbcc4f
player31-kks <keumks789@naver.com>
2023-07-08 10:34:58 +0900

    chore : eslintrc 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,7 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'import'],
   root: true,
   env: {
     node: true,
@@ -21,5 +18,21 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index', 'object', 'type'],
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            pattern: '@?(shared|test|util)/**',
+            group: 'internal',
+          },
+        ],
+      },
+    ],
   },
 };
