65da481e74fd0dbdc023362bc9502d7e618ef731
Nhahan <kisy324@naver.com>
2023-07-18 23:48:08 +0900

    feat: saveUser

--- src/modules/user/infra/db/user.repository.ts
+++ src/modules/user/infra/db/user.repository.ts
@@ -1,7 +1,7 @@
 import { Repository } from 'typeorm';
 
 import { UserDomain } from '@module/user/domain/domain/user.domain';
-import { mapToDomain } from '@shared/domain/mapper';
+import { mapToDomain, mapToEntity } from '@shared/domain/mapper';
 import { CustomRepository } from '@shared/orm/typeorm-ex.decorator';
 
 import { UserEntity } from './entity/user.entity';
@@ -11,5 +11,11 @@ export class UserRepository extends Repository<UserEntity> {
   async findById(id: number): Promise<UserDomain | undefined> {
     const entity = await this.findOne({ where: { id } });
     return entity ? mapToDomain<UserEntity, UserDomain>(entity, UserDomain) : undefined;
+  }
+
+  async saveUser(user: UserDomain): Promise<UserDomain> {
+    const entity = mapToEntity(user, UserEntity);
+    const savedEntity = await this.save(entity);
+    return mapToDomain<UserEntity, UserDomain>(savedEntity, UserDomain);
   }
 }
