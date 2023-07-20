faa38729aa939873d1dcec68dc95dad3d51682c5
Nhahan <kisy324@naver.com>
2023-07-20 23:30:02 +0900

    feat: CreateUserInput, create-user usecase 정의

--- src/modules/user/facade/create/create-user.usecase.ts
+++ src/modules/user/facade/create/create-user.usecase.ts
@@ -3,9 +3,4 @@ import { UserDomainService } from '@module/user/domain/service/user.domain.servi
 
 export class CreateUserUsecase {
   constructor(private userDomainService: UserDomainService) {}
-
-  async execute(input: CreateUserInput) {
-    // const user = UserDomain.of(input);
-    // await this.userDomainService;
-  }
 }
